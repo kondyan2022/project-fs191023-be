6fe5826bc7e95760955a4f21866b25d3a8798a33
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-10-24 00:31:31 +0300

    Add diary model

--- models/diary.js
+++ models/diary.js
@@ -3,14 +3,103 @@ const Joi = require("joi");
 
 const { handleMongooseError } = require("../helpers");
 
-const diarySchema = new Schema({
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: "user",
+const productItemSchema = new Schema({
+  title: {
+    type: String,
     required: true,
   },
-  date: {
-    type: Date,
+  category: {
+    type: String,
+    required: true,
+  },
+  weight: {
+    type: Number,
+    min: 1,
+    required: true,
+  },
+  calories: {
+    min: 1,
+    type: Number,
+    required: true,
+  },
+  recommend: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const exerciseItemSchema = new Schema({
+  bodyPart: {
+    type: String,
+    required: true,
+  },
+  equipment: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  burnedCalories: {
+    type: Number,
+    min: 1,
+    required: true,
+  },
+  time: {
+    type: Number,
+    min: 1,
     required: true,
   },
 });
+
+const diarySchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    date: {
+      type: String,
+      min: 8,
+      max: 8,
+      required: true,
+    },
+    DSN: { type: Number, required: true },
+    BMR: { type: Number, required: true },
+    products: [productItemSchema],
+    exercises: [exerciseItemSchema],
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+    toJSON: { virtuals: true },
+    virtuals: {
+      burnedCalories: {
+        get() {
+          return this.exercises.reduce(
+            (acc, { burnedCalories }) => acc + burnedCalories,
+            0
+          );
+        },
+      },
+      consumedCalories: {
+        get() {
+          return this.products.reduce((acc, { calories }) => acc + calories, 0);
+        },
+      },
+      doneExercisesTime: {
+        get() {
+          return this.exercises.reduce((acc, { time }) => acc + time, 0);
+        },
+      },
+    },
+  }
+);
+
+diarySchema.post("save", handleMongooseError);
