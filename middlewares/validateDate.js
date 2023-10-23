e9128e7739e611c4247e3a111797a4f627d445f7
Viacheslav Dziubenko <97623457+MrViachesav25@users.noreply.github.com>
2023-10-24 01:00:27 +0300

    add middle

--- middlewares/validateDate.js
+++ middlewares/validateDate.js
@@ -1,20 +1,28 @@
 const { HttpError } = require("../helpers");
 
-const validateDate = (schema) => {
-    const func = (req, res, next) => {
-      const { value, error } = schema.validate(req.query);
-      if (error) {
-        next(HttpError(400, `Invalid query params: ${error.message}`));
-      } else {
-        if (typeof value.date === 'number') {
-          value.date = new Date(value.date).toISOString();
+const validateDate = (req, res, next) => {
+      const { date } = req.body;
+      if(date) {
+        let formattedDate = '';
+        if (typeof date === 'string') {
+          const dateObj = new Date(date);
+          if(isNaN(dateObj.getTime())) return next(HttpError(400, 'Invalid date format'));
+          const year = dateObj.getFullYear();
+          const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+          const day = String(dateObj.getDate()).padStart(2, '0');
+
+          formattedDate = `${year}${month}${day}`;
         }
-        req.query = { ...req.query, ...value };
-        next();
+        else if (date instanceof Date) {
+          const year = date.getFullYear();
+          const month = String(date.getMonth() + 1).padStart(2, '0');
+          const day = String(date.getDate()).padStart(2, '0');
+          formattedDate = `${year}${month}${day}`;
+        }
+        req.body.date = formattedDate;
       }
-    };
-  
-    return func;
-  };
+      next();
+}
+
 
 module.exports = validateDate;
